e51ef8604637e762a81a19be545b28b668e14a00
vikash <vikash170294@gmail.com>
2021-11-26 15:23:47 +0530

    Updated MOdels.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 # Create your models here.
+
+
+class User(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        unique = ('email')
+        fields = ('first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2')
